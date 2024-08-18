61252229a9761d2dddefeda1ac4246a5b8131d80
Rhylaz <103692145+Rhylaz@users.noreply.github.com>
2024-08-18 06:30:59 +0200

    Added a Functional Component
    Creating a Functional Component:
    Define a function that returns JSX.
    
    Exporting: Use export default to make it available for import.
    Importing: Use import in the parent component or file where you want to use it.
    
    Using the Component: Include the component in your JSX and pass any required props.

---
+++ app/components/testing.jsx
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Marcelo" />
+      <Greeting name="Daniel" />
+    </div>
+  );
+}
+
+export default App;
